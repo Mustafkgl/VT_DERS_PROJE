c9b4cfe99fa4d4e97287eb0bd6457ff4246d55c8
Mustafa Akgul <mustafakgl@proton.me>
2023-03-07 23:31:54 +0300

    Add PostgreSQL connection with SQLAlchemy

--- update_admin_password.py
+++ update_admin_password.py
@@ -5,3 +5,9 @@ import psycopg2
 conn = psycopg2.connect(
     dbname="library_db",
     user="library_user",
+    password="library123",
+    host="localhost",
+    port="5432"
+)
+
+cur = conn.cursor()
