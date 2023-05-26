4e5784cb868e339501eee3f68b07b9c34a57c49f
Mustafa Akgul <mustafakgl@proton.me>
2023-05-26 17:08:39 +0300

    Implement rate limiting middleware

--- config.py
+++ config.py
@@ -5,3 +5,12 @@ load_dotenv()
 
 class Config:
     """Uygulama yapılandırma sınıfı"""
+
+    # Secret key for JWT
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-in-production')
+
+    # Database configuration
+    DB_USER = os.getenv('DB_USER', 'library_user')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'library123')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_PORT = os.getenv('DB_PORT', '5432')
