c3c4342e48ebcf9b1da6b70af56e7a89566f342c
Mustafa Akgul <mustafakgl@proton.me>
2023-05-27 19:05:10 +0300

    Add structured audit logging

--- config.py
+++ config.py
@@ -14,3 +14,11 @@ class Config:
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'library123')
     DB_HOST = os.getenv('DB_HOST', 'localhost')
     DB_PORT = os.getenv('DB_PORT', '5432')
+    DB_NAME = os.getenv('DB_NAME', 'library_db')
+
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # JWT Configuration
+    JWT_ALGORITHM = 'HS256'
+    JWT_EXPIRATION_HOURS = 24
