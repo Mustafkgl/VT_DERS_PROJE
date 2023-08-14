a37f6d66f02de9a9a80a73f34a354e2ac9722a18
Mustafa Akgul <mustafakgl@proton.me>
2023-08-14 14:17:17 +0300

    Add due date tracking

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from app import create_app
 from app.models import db
+from config import Config
+
+
+class TestConfig(Config):
+    """Test configuration"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # In-memory database for tests
+    SECRET_KEY = 'test-secret-key'
+    WTF_CSRF_ENABLED = False
+
+
+@pytest.fixture(scope='function')
+def app():
+    """Create application for testing"""
