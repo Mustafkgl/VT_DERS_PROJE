6072c8a13492033b0a62f17dfe76762f8afa1fc4
Mustafa Akgul <mustafakgl@proton.me>
2023-11-14 17:40:04 +0300

    Add OpenAPI documentation

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -37,3 +37,41 @@ class PasswordValidator:
             return False, 'Şifre en az 8 karakter olmalı'
 
         if len(password) > 128:
+            return False, 'Şifre en fazla 128 karakter olabilir'
+
+        # Büyük harf kontrolü
+        if not re.search(r'[A-Z]', password):
+            return False, 'Şifre en az 1 büyük harf içermeli'
+
+        # Küçük harf kontrolü
+        if not re.search(r'[a-z]', password):
+            return False, 'Şifre en az 1 küçük harf içermeli'
+
+        # Rakam kontrolü
+        if not re.search(r'\d', password):
+            return False, 'Şifre en az 1 rakam içermeli'
+
+        # Özel karakter kontrolü
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+            return False, 'Şifre en az 1 özel karakter içermeli (!@#$%^&*...)'
+
+        # Yaygın şifre kontrolü
+        if password.lower() in PasswordValidator.COMMON_PASSWORDS:
+            return False, 'Bu şifre çok yaygın kullanılıyor, lütfen daha güçlü bir şifre seçin'
+
+        return True, ''
+
+
+class InputValidator:
+    """Girdi doğrulama ve güvenlik kontrolleri"""
+
+    @staticmethod
+    def sanitize_text(text, max_length=None):
+        """
+        Metni temizle ve XSS saldırılarına karşı koruma sağla
+        """
+        if not text:
+            return text
+
+        # HTML karakterlerini escape et
+        sanitized = escape(str(text).strip())
