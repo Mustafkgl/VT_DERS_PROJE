16be3060143eca42ef0121c27f642108a6dbf386
Mustafa Akgul <mustafakgl@proton.me>
2023-12-16 17:48:20 +0300

    Add book cover image upload

--- app/utils/security_headers.py
+++ app/utils/security_headers.py
@@ -13,3 +13,17 @@ def add_security_headers(app: Flask):
     - X-Frame-Options: Clickjacking koruması
     - X-XSS-Protection: XSS koruması (eski tarayıcılar için)
     - Strict-Transport-Security: HTTPS zorunluluğu
+    - Content-Security-Policy: XSS ve injection saldırı koruması
+    - Referrer-Policy: Referrer bilgisi gizliliği
+    """
+
+    @app.after_request
+    def set_security_headers(response):
+        # MIME type sniffing koruması
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+
+        # Clickjacking koruması
+        response.headers['X-Frame-Options'] = 'DENY'
+
+        # XSS koruması (eski tarayıcılar için)
+        response.headers['X-XSS-Protection'] = '1; mode=block'
