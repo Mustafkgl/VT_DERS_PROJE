dae5e3c68e2f8c39f4ad0a1deff854d19581db01
Mustafa Akgul <mustafakgl@proton.me>
2024-09-17 10:42:24 +0300

    Add structured audit logging

--- app/repositories/fine_repository.py
+++ app/repositories/fine_repository.py
@@ -11,3 +11,17 @@ class FineRepository:
     @staticmethod
     def find_by_user(user_id):
         """Kullanıcının tüm cezalarını getir"""
+        return Fine.query.filter_by(user_id=user_id).all()
+
+    @staticmethod
+    def find_unpaid_by_user(user_id):
+        """Kullanıcının ödenmemiş cezalarını getir"""
+        return Fine.query.filter_by(user_id=user_id, paid=False).all()
+
+    @staticmethod
+    def find_by_borrowing(borrowing_id):
+        """Ödünç alma kaydına ait cezayı getir"""
+        return Fine.query.filter_by(borrowing_id=borrowing_id).first()
+
+    @staticmethod
+    def get_all():
