6411c1e96b89d366bf6e88994d7d65050701fe50
Mustafa Akgul <mustafakgl@proton.me>
2025-01-15 18:31:58 +0300

    Add user deactivation endpoint

--- app/models/book.py
+++ app/models/book.py
@@ -7,3 +7,12 @@ class Book(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    isbn = db.Column(db.String(20), unique=True)
+    publisher = db.Column(db.String(100))
+    publication_year = db.Column(db.Integer)
+    total_copies = db.Column(db.Integer, nullable=False, default=1)
+    available_copies = db.Column(db.Integer, nullable=False, default=1)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # İlişkiler
