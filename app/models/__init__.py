24447b7c592b2fde4e90c046f8d5b3428a50d6f3
Mustafa Akgul <mustafakgl@proton.me>
2025-01-27 11:42:51 +0300

    Add book cover image upload

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,1 @@
 from app.models.user import db, User
-from app.models.book import Book
-from app.models.borrowing import Borrowing
-from app.models.fine import Fine
-
-__all__ = ['db', 'User', 'Book', 'Borrowing', 'Fine']
