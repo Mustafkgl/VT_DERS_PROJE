b58f0987d46c2396d835c52fdc40033154032cd0
Mustafa Akgul <mustafakgl@proton.me>
2025-03-31 20:11:11 +0300

    Add request ID to all responses

--- app/controllers/fine_controller.py
+++ app/controllers/fine_controller.py
@@ -9,3 +9,14 @@ fine_bp = Blueprint('fine', __name__, url_prefix='/api/fines')
 
 @fine_bp.route('/my', methods=['GET'])
 @token_required
+def get_my_fines(current_user):
+    """Kendi cezalarımı getir"""
+    result = FineService.get_user_fines(current_user['user_id'])
+    return jsonify(result), 200
+
+@fine_bp.route('/my/unpaid', methods=['GET'])
+@token_required
+def get_my_unpaid_fines(current_user):
+    """Ödenmemiş cezalarımı getir"""
+    result = FineService.get_unpaid_fines(current_user['user_id'])
+    return jsonify(result), 200
